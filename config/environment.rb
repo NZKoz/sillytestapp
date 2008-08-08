2f66594d06b193c064e3bb52677391a4827f71c8
Michael Koziarski <michael@koziarski.com>
2008-08-08 16:53:15 +0200

    Add 'java' environment

--- config/environment.rb
+++ config/environment.rb
@@ -66,3 +66,7 @@ Rails::Initializer.run do |config|
   # Activate observers that should always be running
   # config.active_record.observers = :cacher, :garbage_collector
 end
+
+Dispatcher.after_dispatch do
+  ActiveRecord::Base.clear_active_connections!
+end
