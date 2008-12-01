c8d2668b3555b934762e53609aad3db84b41d556
Jeremy Kemper <jeremy@bitsweat.net>
2008-11-30 17:41:22 -0800

    Repeat a few times

--- test/performance/greetings_test.rb
+++ test/performance/greetings_test.rb
@@ -2,7 +2,9 @@ require 'performance/test_helper'
 
 class GreetingsTest < ActionController::PerformanceTest
   def test_index
-    get greetings_path
-    assert_response :ok
+    100.times do
+      get greetings_path
+      assert_response :ok
+    end
   end
 end
